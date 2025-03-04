ba5f7a532d6376a04a635d570fd5683622111fb5
Narasimha <narasimhaanguluri001@gmail.com>
2025-03-04 13:43:37 +0530

    "add: main function to run the Spring application"

---
+++ src/main/java/org/techtricks/artisanPlatform/services/JwtService.java
@@ -0,0 +1,44 @@
+package org.techtricks.artisanPlatform.services;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY="null";
+
+    // Generate signing key
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    // Extract username from token
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    // Validate token expiration
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    // Extract claims
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    // Validate token
+    public boolean validateToken(String token, String username) {
+        return (extractUsername(token).equals(username) && !isTokenExpired(token));
+    }
+}
