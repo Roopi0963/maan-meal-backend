ca9e31b29b062fd50b083c50f4927faae53cb791
Narasimha <narasimhaanguluri001@gmail.com>
2025-03-04 13:30:44 +0530

    "add: Added Artisan model"

---
+++ src/main/java/org/techtricks/artisanPlatform/exceptions/ArtisanAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package org.techtricks.artisanPlatform.exceptions;
+
+public class ArtisanAlreadyExistsException extends Exception {
+    public ArtisanAlreadyExistsException(String message) {
+        super(message);
+    }
+}
