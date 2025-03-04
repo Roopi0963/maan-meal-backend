6667f3c697becd48dad474f5d1b6243bd5a06515
Narasimha <narasimhaanguluri001@gmail.com>
2025-03-04 13:43:20 +0530

    "add: main function to run the Spring application"

---
+++ src/main/java/org/techtricks/artisanPlatform/ArtisanPlatformApplication.java
@@ -0,0 +1,16 @@
+package org.techtricks.artisanPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisanPlatformApplication {
+
+	//main function
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(ArtisanPlatformApplication.class, args);
+	}
+
+}
