183e85dafe6d910713b9792ffb12cd2a57d192ca
Narasimha <narasimhaanguluri001@gmail.com>
2025-03-04 13:48:11 +0530

    "add:Added LoginRequest DTO for authentication"

---
+++ src/main/java/org/techtricks/artisanPlatform/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package org.techtricks.artisanPlatform.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String email;
+    private String password;
+}
