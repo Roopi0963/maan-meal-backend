6c53f072512502f62ef04fa86c756341cc44b1f2
Narasimha <narasimhaanguluri001@gmail.com>
2025-03-04 13:42:23 +0530

    "add: added the exception to handle to the user not found exceptions"

---
+++ src/main/java/org/techtricks/artisanPlatform/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package org.techtricks.artisanPlatform.exceptions;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
