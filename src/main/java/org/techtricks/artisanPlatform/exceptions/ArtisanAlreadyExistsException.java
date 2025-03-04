682a524e8585b50735a682df1918ec1cb30fa801
Narasimha <narasimhaanguluri001@gmail.com>
2025-03-04 13:32:42 +0530

    "add:Added exception handling for existing Artisan"

--- src/main/java/org/techtricks/artisanPlatform/exceptions/ArtisanAlreadyExistsException.java
+++ src/main/java/org/techtricks/artisanPlatform/exceptions/ArtisanAlreadyExistsException.java
@@ -3,5 +3,6 @@ package org.techtricks.artisanPlatform.exceptions;
 public class ArtisanAlreadyExistsException extends Exception {
     public ArtisanAlreadyExistsException(String message) {
         super(message);
+
     }
 }
