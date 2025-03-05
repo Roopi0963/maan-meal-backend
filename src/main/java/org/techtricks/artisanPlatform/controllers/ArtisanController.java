0ce7b8a2a032b8d827d0595b94956abf9c1f3302
Narasimha <narasimhaanguluri001@gmail.com>
2025-03-06 01:01:16 +0530

    "update: update the artisan controller layer with getByEmail and delete artisan mapping"

--- src/main/java/org/techtricks/artisanPlatform/controllers/ArtisanController.java
+++ src/main/java/org/techtricks/artisanPlatform/controllers/ArtisanController.java
@@ -1,6 +1,7 @@
 package org.techtricks.artisanPlatform.controllers;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.techtricks.artisanPlatform.exceptions.ArtisanAlreadyExistsException;
@@ -9,6 +10,7 @@ import org.techtricks.artisanPlatform.models.Artisan;
 import org.techtricks.artisanPlatform.services.ArtisanService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/artisan")
@@ -19,11 +21,19 @@ public class ArtisanController {
     public ArtisanController(ArtisanService artisanService) {
         this.artisanService = artisanService;
     }
-
     @PostMapping("/signup")
-    public ResponseEntity<Artisan> registerArtisan(@RequestBody Artisan artisan) throws ArtisanAlreadyExistsException {
-        Artisan registerArtisan = artisanService.saveArtisan(artisan);
-        return ResponseEntity.ok(registerArtisan);
+    public ResponseEntity<?> registerArtisan(@RequestBody Artisan artisan) {
+        try {
+            Artisan registeredArtisan = artisanService.saveArtisan(artisan);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(Map.of("message", "Artisan registered successfully", "artisan", registeredArtisan));
+        } catch (ArtisanAlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(Map.of("error", e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("error", "Something went wrong. Please try again."));
+        }
     }
 
 
@@ -37,5 +47,18 @@ public class ArtisanController {
     public ResponseEntity<Artisan> getByUsername(@RequestParam("username") String username) throws ArtisanNotFoundException {
         Artisan artisan = artisanService.getByUsername(username);
         return ResponseEntity.ok(artisan);
+    }
+
+    @GetMapping("/getByEmail")
+    public ResponseEntity<Artisan> getByEmail(@RequestParam("email") String email) throws ArtisanNotFoundException {
+        Artisan artisan = artisanService.getByEmail(email);
+        return ResponseEntity.ok(artisan);
+    }
+
+    @DeleteMapping("/deleteAccount")
+    public boolean deleteByEmail(@RequestParam String email) throws ArtisanNotFoundException {
+        Artisan artisan = artisanService.getByEmail(email);
+        return artisanService.deleteByEmail(email);
+
     }
 }
