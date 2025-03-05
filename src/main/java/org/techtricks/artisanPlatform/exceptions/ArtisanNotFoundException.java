9ab2356674c46406f12c75901041db5f91d71420
Narasimha <narasimhaanguluri001@gmail.com>
2025-03-05 19:30:11 +0530

    "add:Added the artisan NotFoundException"

---
+++ src/main/java/org/techtricks/artisanPlatform/exceptions/ArtisanNotFoundException.java
@@ -0,0 +1,7 @@
+package org.techtricks.artisanPlatform.exceptions;
+
+public class ArtisanNotFoundException extends Exception {
+    public ArtisanNotFoundException(String message) {
+        super();
+    }
+}
