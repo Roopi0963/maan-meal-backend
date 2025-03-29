3007feff4616716807b3d5e48835a349e6338ec3
Narasimha123001 <narasimhaanguluri2003@gmail.com>
2025-03-29 20:10:01 +0530

    "add: added the cartItem model class"

---
+++ src/main/java/org/techtricks/artisanPlatform/models/CartItem.java
@@ -0,0 +1,34 @@
+package org.techtricks.artisanPlatform.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "cart_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "cart")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+    private double discount;
+    private double productPrice;
+
+}
