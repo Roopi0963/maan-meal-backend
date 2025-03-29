bcf61fc542a9ea80260e8f6b7a1797dc6d6d0449
Narasimha123001 <narasimhaanguluri2003@gmail.com>
2025-03-29 20:11:39 +0530

    "add: added the cart services"

---
+++ src/main/java/org/techtricks/artisanPlatform/services/CartService.java
@@ -0,0 +1,30 @@
+package org.techtricks.artisanPlatform.services;
+
+
+import org.springframework.stereotype.Service;
+import org.techtricks.artisanPlatform.dto.CartDTO;
+import org.techtricks.artisanPlatform.exceptions.ResourceNotFoundException;
+import org.techtricks.artisanPlatform.exceptions.UserNotFoundException;
+import org.techtricks.artisanPlatform.models.Cart;
+import org.techtricks.artisanPlatform.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface CartService {
+
+    public Cart createCartForUser(User user);
+    CartDTO addProductToCart(Long cartId, Long productId, Integer quantity) throws ResourceNotFoundException, UserNotFoundException;
+
+
+    List<CartDTO> getAllCarts();
+
+    public CartDTO getCart(String emailId) throws ResourceNotFoundException;
+
+    CartDTO updateProductQuantityInCart(Long cartId, Long productId, Integer quantity) throws ResourceNotFoundException;
+
+    void updateProductInCarts(Long cartId, Long productId) throws ResourceNotFoundException;
+
+    String deleteProductFromCart(Long cartId, Long productId) throws ResourceNotFoundException;
+}
