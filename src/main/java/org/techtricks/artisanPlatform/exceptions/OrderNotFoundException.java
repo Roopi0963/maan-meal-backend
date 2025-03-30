4c0c95729125530611a3f20cc2d53810efc3e8f2
Narasimha123001 <narasimhaanguluri2003@gmail.com>
2025-03-30 21:03:54 +0530

    "add: added the order not found for receptions handling "

---
+++ src/main/java/org/techtricks/artisanPlatform/exceptions/OrderNotFoundException.java
@@ -0,0 +1,12 @@
+package org.techtricks.artisanPlatform.exceptions;
+
+
+
+public class OrderNotFoundException extends Exception {
+
+
+    public OrderNotFoundException(String message) {
+
+        super(message);
+    }
+}
