c30771c3a221a20e5063bdf4cf14f394d788ca34
Narasimha123001 <narasimhaanguluri2003@gmail.com>
2025-04-02 00:39:10 +0530

    "add: added the Address Not Found Exceptions for handling the exceptions""

---
+++ src/main/java/org/techtricks/artisanPlatform/exceptions/AddressNotFoundException.java
@@ -0,0 +1,7 @@
+package org.techtricks.artisanPlatform.exceptions;
+
+public class AddressNotFoundException extends Exception {
+    public AddressNotFoundException(String message) {
+        super();
+    }
+}
