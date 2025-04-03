8be41d172bd3f70bce7df795a481c42390e5609e
Narasimha123001 <narasimhanguluri001@gmail.com>
2025-04-03 22:33:10 +0530

    "update: updated the Query for the Order Repository"

--- src/main/java/org/techtricks/artisanPlatform/repositories/OrderRepository.java
+++ src/main/java/org/techtricks/artisanPlatform/repositories/OrderRepository.java
@@ -1,6 +1,9 @@
 package org.techtricks.artisanPlatform.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.techtricks.artisanPlatform.models.Order;
 import org.techtricks.artisanPlatform.models.OrderStatus;
@@ -10,10 +13,13 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     List<Order> findByUser(User user);
     List<Order> findByUserId(Long userId);
     List<Order> findByStatus(OrderStatus status);
     Optional<Order> findById(Long orderId);
+
+    @EntityGraph(attributePaths = {"orderItems"})
+    @Query("SELECT o From Order o JOIN FETCH o.orderItems where  o.user.id = :userId")
+    List<Order> findByUserIdWithOrderItems(@Param("userId") Long userId);
 }
