dfaf3d94fee824268e4f2157831b8822f868ffb9
Narasimha123001 <narasimhanguluri001@gmail.com>
2025-04-03 22:27:55 +0530

    "update: enable the jpaRepositorie"

--- src/main/java/org/techtricks/artisanPlatform/ArtisanPlatformApplication.java
+++ src/main/java/org/techtricks/artisanPlatform/ArtisanPlatformApplication.java
@@ -5,7 +5,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackages ="org.techtricks.artisanPlatform.repositories" )
 @SpringBootApplication
 public class ArtisanPlatformApplication {
 
