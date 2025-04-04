a34b874b2c31139b78f7b0582714f347c901f701
Narasimha123001 <narasimhanguluri001@gmail.com>
2025-04-04 19:55:22 +0530

    "updated:added the Order Date to LocalTimeDate"

--- src/main/java/org/techtricks/artisanPlatform/dto/OrderDTO.java
+++ src/main/java/org/techtricks/artisanPlatform/dto/OrderDTO.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.techtricks.artisanPlatform.models.Order;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @ToString
@@ -15,9 +18,20 @@ import java.util.List;
 
 public class OrderDTO {
     private Long orderId;
-    private Date orderDate;
+    private LocalDateTime orderDate;
     private String status;
     private double totalAmount;
     private List<OrderItemDTO> orderItems;
     private String address;
+
+    public OrderDTO(Order order) {
+        this.orderId = order.getId();
+        this.orderDate = order.getOrderDate();
+        this.totalAmount = order.getTotalAmount();
+        this.status = String.valueOf(order.getStatus());
+        this.address = order.getAddress().toString(); // Assuming `Address` has a `toString()`
+        this.orderItems = order.getOrderItems().stream()
+                .map(OrderItemDTO::new)
+                .collect(Collectors.toList());
+    }
 }
