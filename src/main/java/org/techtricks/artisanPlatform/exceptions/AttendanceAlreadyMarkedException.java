a2b83bd2d76198ee1ff6d049516bc3e051ab9d85
Narasimha123001 <narasimhanguluri001@gmail.com>
2025-04-08 14:15:03 +0530

    "add added the attendance already marks exceptions"

---
+++ src/main/java/org/techtricks/artisanPlatform/exceptions/AttendanceAlreadyMarkedException.java
@@ -0,0 +1,7 @@
+package org.techtricks.artisanPlatform.exceptions;
+
+public class AttendanceAlreadyMarkedException extends Exception {
+    public AttendanceAlreadyMarkedException(String message) {
+        super(message);
+    }
+}
